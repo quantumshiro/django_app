a4068f956bddba85867dcb4e7279b03dfc3d581e
sacky3105 <kgglee.114.baritone@gmail.com>
2024-04-30 23:43:48 +0900

    タスク編集機能を実装

--- app/todo/forms.py
+++ app/todo/forms.py
@@ -1,8 +1,4 @@
-from typing import Any, Mapping
 from django import forms
-from django.core.files.base import File
-from django.db.models.base import Model
-from django.forms.utils import ErrorList
 from .models import Folder, Task
 
 class FolderForm(forms.ModelForm):
@@ -19,15 +15,15 @@ class FolderForm(forms.ModelForm):
         
 class TaskForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(FolderForm, self).__init__(*args, **kwargs)
+        super(TaskForm, self).__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs = {
                 'class': 'form-control'
             }
     class Meta:
-        model = Task
         STATUS_CHOICES = [(1, '未完了'),(2, '作業中'),(3, '完了')]
-        fields = ('title', 'status', 'due_date')
+        model = Task
+        fields = ('title', 'status','due_date')
         labels = {
             'title': 'タスク名',
             'status': '状態',
