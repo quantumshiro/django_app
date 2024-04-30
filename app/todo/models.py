1e45c26c834d9929b567cb92c89648f341a57658
sacky3105 <kgglee.114.baritone@gmail.com>
2024-04-30 19:25:11 +0900

    フォルダ一覧ページを作成

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,12 +1,12 @@
+# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Folder(models.Model):
-    title = models.CharField(max_length = 200)
+    title = models.CharField(max_length=20)
     created_at = models.DateTimeField(default=timezone.now)
-    
+
     def __str__(self):
         return self.title
-
-# Create your models here.
--- app/todo/views.py
+++ app/todo/views.py
@@ -1,9 +1,8 @@
+# Create your views here.
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Folder
 
 def index(request, id):
     folders = Folder.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
-    return reader(request, 'index.html', {'folders' : folders})
-
-# Create your views here.
+    return render(request, 'index.html', {'folders': folders})
