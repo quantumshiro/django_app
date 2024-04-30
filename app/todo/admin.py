c63dccfbdfcfac1de12cd6cc40b00d73171db044
sacky3105 <kgglee.114.baritone@gmail.com>
2024-04-30 20:19:35 +0900

    タスク一覧をページに追加

--- app/todo/admin.py
+++ app/todo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Folder
+from .models import Folder, Task
 
 # Register your models here.
 admin.site.register(Folder)
+admin.site.register(Task)
--- app/todo/models.py
+++ app/todo/models.py
@@ -10,3 +10,22 @@ class Folder(models.Model):
 
     def __str__(self):
         return self.title
+
+class Task(models.Model):
+    STATUS_CHOICES = [(1, '未完了'), (2, '作業中'), (3, '完了')]
+    
+    title = models.CharField(max_length=100)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    due_date = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    folder_id = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    
+    def publish(self):
+        self.updated_at = timezone.now()
+        self.save()
+        
+    def __str__(self):
+        return self.title
+    
+    
