983f0731bed290aa068c579aa9f479966fa12961
quantumshiro <hiro.1nakanishi@gmail.com>
2024-04-30 20:57:10 +0900

    fix: return statement

--- app/todo/views.py
+++ app/todo/views.py
@@ -6,7 +6,7 @@ from .models import Folder, Task
 def index(request, id):
     #すべてのフォルダを取得する
     folders = Folder.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
-    return render(request, 'index.html', {'folders': folders})
+    
     #選ばれたフォルダを取得する
     current_folder = get_object_or_404(Folder, id=id)
     #選ばれたフォルダのタスクを取得する
