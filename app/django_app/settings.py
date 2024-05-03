ff3268ef582af70a109362b1b4edcd182d2b72cc
sacky3105 <kgglee.114.baritone@gmail.com>
2024-05-03 15:10:07 +0900

    ALLOWED_HOSTS名を記入

--- app/django_app/settings.py
+++ app/django_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@ja&&g0*)rrsn67x9jpsmcit)60i5uyoe!%(xdbt%&_&ymi+iz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [django.todo-sample1324.jp]
 
 
 # Application definition
