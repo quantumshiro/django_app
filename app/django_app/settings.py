bd20bd86c14d9a7edcd5d43c8fa0c2f4041862a2
sacky3105 <kgglee.114.baritone@gmail.com>
2024-05-03 21:26:30 +0900

    fix : change the setting of DEBUG

--- app/django_app/settings.py
+++ app/django_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ja&&g0*)rrsn67x9jpsmcit)60i5uyoe!%(xdbt%&_&ymi+iz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
