8271ba98ad0c6dc7edac6f08425f37c4775d0d0c
TAKUCH <37340365+sacky3105@users.noreply.github.com>
2024-05-04 10:14:01 +0900

    Update urls.py
    fix : Change urlpatterns

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -21,5 +21,5 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('folders/', include('todo.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
